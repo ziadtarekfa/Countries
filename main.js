504bca5c57af962430698b50e60684466e745a2b
Ziad Tarek <ziadtarekfa@gmail.com>
2022-09-09 21:05:59 +0200

    added the details page

--- main.js
+++ main.js
@@ -22,30 +22,33 @@ async function getAllCountries() {
     return data;
 }
 
-let i = 0;
-getAllCountries().then((data) => {
-    console.log("we got data successfully", data[0]);
+
+function showCountries() {
+    getAllCountries().then((data) => {
+        console.log("we got data successfully", data[0]);
 
 
-    data.map((country) => {
-        const card = document.createElement("div");
-        card.className = 'card';
-        card.addEventListener("click", () => handleCardClick(country));
-        card.innerHTML =
-            `<div class="card-image">
-            <img src="${country.flags.svg}" alt="Egypt flag" />
-        </div>
-        <div class="card-content">
-            <h1>${country.name}</h1>
-            <p><strong>Population: </strong>${country.population}</p>
-            <p id="country-region"><strong>Region: </strong>${country.region}</p>
-            <p><strong>Capital: </strong>${country.capital}</p>
-        </div>`;
+        data.map((country) => {
+            const card = document.createElement("div");
+            card.className = 'card';
+            card.addEventListener("click", () => handleCardClick(country));
+            card.innerHTML =
+                `<div class="card-image">
+                <img src="${country.flags.svg}" alt="Egypt flag" />
+            </div>
+            <div class="card-content">
+                <h1>${country.name}</h1>
+                <p><strong>Population: </strong>${country.population}</p>
+                <p id="country-region"><strong>Region: </strong>${country.region}</p>
+                <p><strong>Capital: </strong>${country.capital}</p>
+            </div>`;
 
-        countriesContainer.append(card);
+            countriesContainer.append(card);
 
+        });
     });
-});
+}
+showCountries();
 
 searchedCountry.addEventListener("input", (e) => {
 
@@ -81,11 +84,59 @@ selectedRegion.addEventListener("input", (e) => {
 });
 
 function handleCardClick(country) {
-    console.log("you clicked");
-    console.log(country);
 
-    window.location.href = "./CountryDetails.html";
+    let backButton = document.getElementsByClassName("back-btn")[0];
+    let filterContainer = document.getElementsByClassName("filter-container")[0];
+    console.log(filterContainer);
 
+    filterContainer.style.setProperty("display", "none");
+    backButton.style.setProperty("display", "block");
+
+    backButton.addEventListener("click", () => {
+
+
+        filterContainer.style.setProperty("display", "flex");
+        backButton.style.display = "none";
+        countriesContainer.innerHTML = ``;
+        countriesContainer.className = 'countries-container';
+        showCountries();
+    });
+
+
+
+
+
+
+
+    countriesContainer.className = "container";
+    countriesContainer.innerHTML =
+        `<section class="flag">
+    <img src="${country.flags.svg}" />
+</section>
+
+<section class="details-content">
+
+    <h1>Belgium</h1>
+    <section class="left-section">
+        <p><strong>Native Name: </strong>${country.name}</p>
+        <p><strong>Population: </strong>${country.population}</p>
+        <p><strong>Region: </strong>${country.region}</p>
+        <p><strong>Sub Region: </strong>${country.subregion}</p>
+        <p><strong>Capital: </strong>${country.capital}</p>
+
+    </section>
+    <section class="right-section">
+        <p><strong>Top Level Domain: </strong>${country.topLevelDomain}</p>
+        <p><strong>Currencies: </strong>${country.currency}</p>
+        <p><strong>Langauges: </strong>Dutch,French,German</p>
+    </section>
+    <section class="border-countries">
+        <h4>Border Countries:</h4>
+        <div>France</div>
+        <div>France</div>
+        <div>France</div>
+    </section>
+</section>`
 
 };
 
